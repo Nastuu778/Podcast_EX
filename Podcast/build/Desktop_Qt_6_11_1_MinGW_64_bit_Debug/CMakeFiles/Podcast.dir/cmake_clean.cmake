17c46cd8932c00a664047f507761c7808f56d74a
Dizmor <likhouzovaae@gmail.com>
2026-07-26 20:51:59 +0300

    Добавляю .gitignore

---
+++ Podcast/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Podcast.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Podcast.dir/main.cpp.obj"
+  "CMakeFiles/Podcast.dir/main.cpp.obj.d"
+  "Podcast.exe"
+  "Podcast.exe.manifest"
+  "Podcast.pdb"
+  "libPodcast.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Podcast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Podcast/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Podcast.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Podcast.
+# This may be replaced when dependencies are built.
